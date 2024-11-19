e3e6a661c022b9fe5fcf7353ba0e0e8c3a0e5ca1
J155917 <J155917@tafe.wa.edu.au>
2024-11-19 14:45:37 +0800

    refactor: handle button links with js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,46 @@
-const navIcon = document.getElementById("nav-icon")
-const navItems = document.getElementById("nav-items")
+document.addEventListener('DOMContentLoaded', () => {
+    const navIcon = document.getElementById("nav-icon")
+    const navItems = document.getElementById("nav-items")
+    const indexProjectBtn = document.getElementById("btn-index-project")
+    const indexContactBtn = document.getElementById("btn-index-contact")
+    const aboutResumeBtn = document.getElementById("btn-about-resume")
+    const resumeProjectsBtn = document.getElementById("btn-resume-projects")
+    const projectsSampleProjectBtn = document.getElementById("btn-projects-sample-project")
 
-navIcon.addEventListener("click", (e) => {
-    navItems.classList.toggle("hidden");
+    console.log(navIcon, navItems, indexProjectBtn, indexContactBtn, aboutResumeBtn, resumeProjectsBtn, projectsSampleProjectBtn);
+
+    navIcon.addEventListener("click", (e) => {
+        navItems.classList.toggle("hidden");
+    })
+
+    if (indexProjectBtn) {
+        indexProjectBtn.addEventListener("click", (e) => {
+            location.href="projects.html"
+        })
+    }
+
+    if (indexContactBtn) {
+        indexContactBtn.addEventListener("click", (e) => {
+            location.href="contact.html"
+        })
+
+    }
+
+    if (aboutResumeBtn) {
+        aboutResumeBtn.addEventListener("click", (e) => {
+            location.href="resume.html"
+        })
+    }
+
+    if (resumeProjectsBtn) {
+        resumeProjectsBtn.addEventListener("click", (e) => {
+            location.href="projects.html"
+        })
+    }
+
+    if (projectsSampleProjectBtn) {
+        projectsSampleProjectBtn.addEventListener("click", (e) => {
+            location.href="my-work.html"
+        })
+    }
 })
